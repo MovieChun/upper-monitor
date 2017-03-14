cc6348a75947d8d459488283eb4291ca506d18cf
Movie <362265679@qq.com>
2017-03-14 11:17:53 +0800

    新建库

---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = "哈哈哈哈";
+            //this.BackColor = Color.FromArgb(0, 0, 0);
+            textBox3.Text = textBox1.Text + "\r\n" + textBox2.Text;
+        }
+    }
+}
